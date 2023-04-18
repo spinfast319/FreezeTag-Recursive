ee6db39e54c143df3e1e598f7eb90261c9440911
spinfast319 <spinfast319@protonmail.com>
2023-04-18 19:19:39 -0400

    refactored script, added main, brought everything in functions

--- FreezeTag-Recursive.py
+++ FreezeTag-Recursive.py
@@ -2,36 +2,130 @@
 
 # FreezeTag-Recursive
 # author: hypermodified
-# A simple python script that loops through a directory and runs the freezetag command on every directory and subdirectory allowing for the folder and file names and all metadata to be recovered if needed with the unfreeze command.
+# A simple python script that loops through a directory and runs the freezetag command on every directory and subdirectory allowing for the folder, filenames and metadata to be recovered if changed.
+# It then checks each directory to make sure a .ftag file is there and logs those that are missing.
+# It can handle albums with artwork folders or multiple disc folders in them.
+# It can also handle specials characters and skips and logs any characters that makes windows fail.
+# It has been tested and works in both Ubuntu Linux and Windows 10.
 
 # Import dependencies
 import os  # Imports functionality that let's you interact with your operating system
 from freezetag import commands # Imports freezetag
+import datetime  # Imports functionality that lets you make timestamps
 
-COUNT = 0
+#  Set your directories here
+album_directory = "M:\PROCESS"  # Which directory do you want to start with?
+log_directory = "M:\PROCESS-LOGS\Logs"  # Which directory do you want the log in?
 
-#  Set your directory here
-directory_to_check = "M:\Python Test Environment\Albums"  # Which directory do you want to start with?
+# Establishes the counters for completed albums and missing origin files
+count = 0
+ftag_create_count = 0
+ftag_missing = 0
+error_message = 0
+
+# Sets up the list of folders missing ftags
+missing_ftag_list = []
+
+# A function to log events
+def log_outcomes(directory, log_name, message):
+    global log_directory
+
+    script_name = "Freeze Tag Recursive Script"
+    today = datetime.datetime.now()
+    log_name = f"{log_name}.txt"
+    album_name = directory.split(os.sep)
+    album_name = album_name[-1]
+    log_path = os.path.join(log_directory, log_name)
+    with open(log_path, "a", encoding="utf-8") as log_name:
+        log_name.write(f"--{today:%b, %d %Y} at {today:%H:%M:%S} from the {script_name}.\n")
+        log_name.write(f"The album folder {album_name} {message}.\n")
+        log_name.write(f"Album location: {directory}\n")
+        log_name.write(" \n")
+        log_name.close()
+
+
+# A function that determines if there is an error
+def error_exists(error_type):
+    global error_message
+
+    if error_type >= 1:
+        error_message += 1  # variable will increment if statement is true
+        return "Warning"
+    else:
+        return "Info"
+
+
+# A function that writes a summary of what the script did at the end of the process
+def summary_text():
+    global count
+    global total_count
+    global good_missing
+    global bad_missing
+    global parse_error
+    global error_message
+    global origin_old
+
+    print("")
+    print(f"This script moved {count} albums out of {total_count} folders examined.")
+    print("This script looks for potential missing files or errors. The following messages outline whether any were found.")
+
+    error_status = error_exists(parse_error)
+    print(f"--{error_status}: There were {parse_error} albums skipped due to not being able to open the yaml. Redownload the yaml file.")
+    error_status = error_exists(origin_old)
+    print(f"--{error_status}: There were {origin_old} origin files that do not have the needed metadata and need to be updated.")
+    error_status = error_exists(bad_missing)
+    print(f"--{error_status}: There were {bad_missing} folders missing an origin files that should have had them.")
+    error_status = error_exists(good_missing)
+    print(f"--Info: Some folders didn't have origin files and probably shouldn't have origin files. {good_missing} of these folders were identified.")
+
+    if error_message >= 1:
+        print("Check the logs to see which folders had errors and what they were.")
+    else:
+        print("There were no errors.")
+
 
 #  A function that gets the directory and then runs the freezetag freeze command plus the directory
-def my_function(directory):
-    global COUNT
-    print("Listing: " + directory)
+def freeze_folder(directory):
+    global count
+    
+    print("")
+    print(f"Listing: {directory}")
     print("\t-" + "\n\t-".join(os.listdir(".")))  # List current working directory
     commands.freeze(directory, False, directory) # Executes the freezetag freeze command on the directory you are in
-    COUNT += 1  # variable will increment every loop iteration
+    count += 1  # variable will increment every loop iteration
 
 
-# Get all the subdirectories of directory_to_check recursively and store them in a list:
-directories = [os.path.abspath(x[0]) for x in os.walk(directory_to_check)]
-directories.remove(os.path.abspath(directory_to_check))  # If you don't want your main directory included
+# The main function that controls the flow of the script
+def main():
+    global count
+    
+    try:
+        # intro text
+        print("")
+        print("In this universe, there's only one absolute. ")
+        print("")
+        print("Part 1: Freezing")
+        
+        # Get all the subdirectories of album_directory recursively and store them in a list:
+        directories = [os.path.abspath(x[0]) for x in os.walk(album_directory)]
+        directories.remove(os.path.abspath(album_directory))  # If you don't want your main directory included
 
-#  Run a loop that goes into each directory identified in the list and runs the function that runs freezetag
-for i in directories:
-    os.chdir(i)  # Change working Directory
-    my_function(i)  # Run your function
+        #  Run a loop that goes into each directory identified in the list and runs the function that runs freezetag
+        for i in directories:
+            os.chdir(i)  # Change working Directory
+            freeze_folder(i)  # Run your function
+            
+        #print("Part 2: Checking for missing freeze files")    
+    
+    finally:
+        # Summary text
+        print("")
+        print("Everything freezes.")
+        # run summary text function to provide error messages
+        print(f"Freeze tag ran {count} times.")
 
-print(f"Freeze tag ran {COUNT} times.")
+if __name__ == "__main__":
+    main()
 
 """      
 In windows freezetag does not create a .ftag file if certain special characters with non normative encodings are in the directory name and this script doesn't either. Some characters it gracefully skips and other characters it crashes. 
