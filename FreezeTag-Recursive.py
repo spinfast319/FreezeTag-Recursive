2dc94b36b388dfcc9f321bc109cc7d40a7bc6dd7
spinfast319 <spinfast319@protonmail.com>
2023-04-18 20:15:18 -0400

    added a check for missing ftag files

--- FreezeTag-Recursive.py
+++ FreezeTag-Recursive.py
@@ -87,6 +87,36 @@ def freeze_folder(directory):
     ftag_create_count += 1  # variable will increment every loop iteration
 
 
+#  A function to identify and log directories missing ftag files
+def missing_ftag(directory):
+    global ftag_missing
+    
+    print(f"Checking for ftag file in {directory}")
+    
+    # Reset file_exists variable
+    file_exists = False
+    
+    # Loop through the directory and check if there is a ftag file
+    for fname in os.listdir(directory):
+        if fname.endswith(".ftag"):
+            file_exists = True
+            break     
+        else: 
+            file_exists = False         
+
+    if file_exists == True:
+        print("--This directory has an ftag file in it.")   
+    else:
+        # list track that was retagged
+        missing_ftag_list.append(directory)
+        print("--This directory is missing an ftag file.")
+        # log the missing ftag file folders that are not likely supposed to be missing
+        print("--Logged missing ftag file.")
+        log_name = "bad-missing-ftag"
+        log_message = "ftag file is missing from a folder that should have one"
+        log_outcomes(directory, log_name, log_message)
+        bad_missing += 1  # variable will increment every loop iteration
+
 # The main function that controls the flow of the script
 def main():
     global count
@@ -95,21 +125,36 @@ def main():
         # intro text
         print("")
         print("In this universe, there's only one absolute. ")
-        print("")
-        print("Part 1: Freezing")
         
         # Get all the subdirectories of album_directory recursively and store them in a list:
         directories = [os.path.abspath(x[0]) for x in os.walk(album_directory)]
         directories.remove(os.path.abspath(album_directory))  # If you don't want your main directory included
 
+        print("")
+        print("Part 1: Freezing")
         #  Run a loop that goes into each directory identified in the list and runs the function that runs freezetag
         for i in directories:
             os.chdir(i)  # Change working Directory
             freeze_folder(i)  # Run your function
             count += 1  # variable will increment every loop iteration
             
+        print("")    
+        print("Part 2: Checking for missing freeze files") 
+        print("")         
+        #  Run a loop that goes into each directory and checks for a freezetag file
+        for i in directories:
+            os.chdir(i)  # Change working Directory
+            missing_ftag(i)  # Run your function
             
-        #print("Part 2: Checking for missing freeze files")    
+        # Print a list of missing directories missing ftag files
+        if missing_ftag_list:
+            print("")
+            print("List of missing directories missing ftag files")
+            for i in (missing_ftag_list):
+                        print(f"--{i}")  
+        else:
+            print("")
+            print("--No folders are missing ftag files.")
     
     finally:
         # Summary text
